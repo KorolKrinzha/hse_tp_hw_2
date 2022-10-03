a4d43f53dd3ed587cc8d34c940423405a7dc03ac
ArtemSmirnovHSE <ar.smirnov2015@mail.ru>
2022-10-03 17:39:14 +0300

    Improved time measure

--- timetests.py
+++ timetests.py
@@ -2,18 +2,13 @@ import timeit
 import numpy as np
 from matplotlib import pyplot as plt
 from NumbersOperator import NumbersOperator
-plt.xlabel('Число данных')
-plt.ylabel('Время исполнения программы')
-testfiles = ['./tests_txt/timetest_1.txt','./tests_txt/timetest_2.txt', './tests_txt/timetest_3.txt']
+number_of_files = 3
+testfiles = ['./tests_txt/timetest_'+str(i)+'.txt' for i in range (1,number_of_files+1)]
+
 numbers_list = [ NumbersOperator(filename) for filename in testfiles]
 numbers_list_size = [operator.getNumberListSize() for operator in numbers_list]
 
 
-def sum_timetest_0(numbers_list):
-    
-    numbers_0 = numbers_list[0]
-    
-    return numbers_0.sum()
     
 # print(numbers_list_size)
 # print(sum_timetest_0(numbers_list))
@@ -24,26 +19,36 @@ from NumbersOperator import NumbersOperator
 testfiles = ['./tests_txt/timetest_1.txt','./tests_txt/timetest_2.txt', './tests_txt/timetest_3.txt']
 numbers_list = [ NumbersOperator(filename) for filename in testfiles]
 """
-sum_timetest_0=  """
-numbers_list[0].sum()
-"""
-sum_timetest_1=  """
-numbers_list[1].sum()
-"""
-sum_timetest_2=  """
-numbers_list[2].sum()
-"""
 
+# sum_timetest_0=  """
+# numbers_list[0].sum()
+# """
+# sum_timetest_1=  """
+# numbers_list[1].sum()
+# """
+# sum_timetest_2=  """
+# numbers_list[2].sum()
+# """
+sum_time_list = []
+for i in range(len(numbers_list)):
+    sum_timetest = f"numbers_list[{i}].sum()"
+    sum_time = timeit.timeit(stmt=sum_timetest, setup=setup, number=10000)
+    sum_time_list.append(sum_time)
 
-sum_time_0 = timeit.timeit(stmt=sum_timetest_0, setup=setup, number=10000)
-sum_time_1 = timeit.timeit(stmt=sum_timetest_1, setup=setup, number=10000)
-sum_time_2 = timeit.timeit(stmt=sum_timetest_2, setup=setup, number=10000)
-print(sum_time_0, sum_time_1, sum_time_2)
-# print(timeit.timeit("[(a, b) for a in (1, 3, 5) for b in (2, 4, 6)]"))
-# x = np.linspace(0, 2.0*np.pi, 101)
-# y = np.sin(x)                        # sine function
+plt.xlabel('Число данных')
+plt.ylabel('Время исполнения программы')
 
+plt.plot(sum_time_list, numbers_list_size, color='r', label='Функция суммы')
+
+min_time_list = []
+for i in range(len(numbers_list)):
+    min_timetest = f"numbers_list[{i}].min()"
+    min_time = timeit.timeit(stmt=min_timetest, setup=setup, number=10000)
+    min_time_list.append(min_time)
+    
+plt.plot(min_time_list, numbers_list_size, color='g', label='Функция минимума')
+
+plt.show()
 
 # plt.plot(x, y, color='r', label='sin')
-# plt.plot(x, y)
 # plt.show()
